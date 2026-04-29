3033fcc1336a58915c63f4ea9de06beaeb137fe0
Asher Azriel Ginting <asherginting3@gmail.com>
2026-04-29 20:00:55 +0700

    fix: railway binding

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ mongoose
   .then(() => {
     console.log("✅ MongoDB connected");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
   })
